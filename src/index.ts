33a343029ea6de3479b131c07b8e7fc21b38b058
blaise2s <blaise.schaeffer@gmail.com>
2020-05-24 20:52:26 -0400

    feat(MASTER): Lots of initial developmemnt

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import * as Admin from 'firebase-admin';
 import * as Express from 'express';
 import * as BodyParser from 'body-parser';
+import { v4 as uuid } from 'uuid';
+
+enum QuestionType {
+  FREE_RESPONSE,
+  MULTIPLE_CHOICE
+}
 
 interface Question {
+  type: QuestionType;
   question: string;
   answer: string;
 }
@@ -13,7 +20,7 @@ interface Section {
 }
 
 interface Questionnaire {
-  uuid: string;
+  id?: string;
   name: string;
   sections: Section[];
 }
@@ -31,15 +38,15 @@ const admin = Admin.initializeApp({
   databaseURL: process.env.DATABASE_URL
 });
 
-const verifyToken = async (jwt: string): Promise<boolean> => {
+const verifyToken = async (jwt: string): Promise<Admin.auth.DecodedIdToken> => {
   return admin
     .auth()
     .verifyIdToken(jwt)
-    .then((_decodedIdToken: Admin.auth.DecodedIdToken) => {
-      return true;
+    .then((decodedIdToken) => {
+      return decodedIdToken;
     })
     .catch(() => {
-      return false;
+      return null;
     });
 };
 
@@ -47,23 +54,25 @@ const extractJwt = (auth: string) => {
   return auth.split(' ')[1];
 };
 
-const extractAndVerifyJwt = async (request: Express.Request) => {
+const extractAndVerifyJwt = async (
+  request: Express.Request
+): Promise<string> => {
   const auth = request.headers.authorization;
   if (auth) {
     const token = extractJwt(auth);
     return verifyToken(token)
-      .then((verified) => {
-        if (verified) {
-          return true;
+      .then((decodedIdToken) => {
+        if (decodedIdToken) {
+          return decodedIdToken.uid;
         } else {
-          return false;
+          return null;
         }
       })
       .catch(() => {
-        return false;
+        return null;
       });
   } else {
-    return Promise.resolve(false);
+    return Promise.resolve(null);
   }
 };
 
@@ -77,49 +86,78 @@ const refOnceVal = async <T>(ref: string) => {
     });
 };
 
-const getQuestionnaires = () => {
-  return refOnceVal<QuestionnaireResponse>('/questionnaires');
+const getQuestionnaires = (uid: string) => {
+  return refOnceVal<QuestionnaireResponse>(`/questionnaires/${uid}`);
 };
 
-const getQuestionnaire = (questionnaireId: string) => {
-  return refOnceVal<Questionnaire>(`/questionnaires/${questionnaireId}`);
+const getQuestionnaire = (uid: string, questionnaireId: string) => {
+  return refOnceVal<Questionnaire>(
+    `/${uid ? `questionnaires/${uid}` : 'published'}/${questionnaireId}`
+  );
 };
 
-const postQuestionnaire = async (questionnaire: Questionnaire) => {
+const postQuestionnaire2 = (uid: string, questionnaire: Questionnaire) => {
   return admin
     .database()
-    .ref('/questionnaires')
-    .push(questionnaire)
-    .then((ref) => ref.key);
+    .ref(`/questionnaires/${uid}/${questionnaire.id}`)
+    .set(questionnaire);
 };
 
-const putQuestionnaire = (
+// TODO?: Does this need more locking down?
+const publishQuestionnaire = (
   questionnaireId: string,
   questionnaire: Questionnaire
 ) => {
   return admin
     .database()
-    .ref(`/questionnaires/${questionnaireId}`)
+    .ref(`/published/${questionnaireId}`)
     .set(questionnaire);
 };
 
-const deleteQuestionnaire = (questionnaireId: string) => {
-  return admin.database().ref(`/questionnaires/${questionnaireId}`).remove();
+const postQuestionnaire = async (uid: string, questionnaire: Questionnaire) => {
+  return admin
+    .database()
+    .ref(`/questionnaires/${uid}`)
+    .push(questionnaire)
+    .then((ref) => ref.key);
+};
+
+const putQuestionnaire = (
+  uid: string,
+  questionnaireId: string,
+  questionnaire: Questionnaire
+) => {
+  return admin
+    .database()
+    .ref(`/questionnaires/${uid}/${questionnaireId}`)
+    .set(questionnaire);
+};
+
+const deleteQuestionnaire = (uid: string, questionnaireId: string) => {
+  return admin
+    .database()
+    .ref(`/questionnaires/${uid}/${questionnaireId}`)
+    .remove();
+};
+
+const unListQuestionnaire = (questionnaireId: string) => {
+  // TODO: Lock Down
+  return admin.database().ref(`/published/${questionnaireId}`).remove();
 };
 
 const questionnaireRequestHandler = (
   request: Express.Request,
   response: Express.Response,
-  fetchFn: (questionnaireId?: string) => Promise<any>,
+  fetchFn: (uid: string, questionnaireId?: string) => Promise<any>,
   questionnaireId?: string
 ) => {
-  extractAndVerifyJwt(request).then((verified) => {
-    if (verified) {
+  extractAndVerifyJwt(request).then((uid) => {
+    if (uid) {
       const fetch = () => {
         if (questionnaireId) {
-          return fetchFn(questionnaireId);
+          return fetchFn(uid, questionnaireId);
         } else {
-          return fetchFn();
+          return fetchFn(uid);
         }
       };
 
@@ -143,6 +181,20 @@ server.get(`${base}/questionnaires`, (request, response) => {
   questionnaireRequestHandler(request, response, getQuestionnaires);
 });
 
+server.get(
+  `${base}/questionnaires/published/:questionnaireId`,
+  (request, response) => {
+    const questionnaireId = request.params.questionnaireId;
+    getQuestionnaire(null, questionnaireId)
+      .then((questionnaire) => {
+        return response.json(questionnaire);
+      })
+      .catch(() => {
+        response.sendStatus(500);
+      });
+  }
+);
+
 server.get(`${base}/questionnaires/:questionnaireId`, (request, response) => {
   const questionnaireId = request.params.questionnaireId;
   questionnaireRequestHandler(
@@ -154,10 +206,12 @@ server.get(`${base}/questionnaires/:questionnaireId`, (request, response) => {
 });
 
 server.post(`${base}/questionnaires`, (request, response) => {
-  extractAndVerifyJwt(request).then((verified) => {
-    if (verified) {
+  extractAndVerifyJwt(request).then((uid) => {
+    if (uid) {
       const questionnaire: Questionnaire = request.body;
-      postQuestionnaire(questionnaire)
+      const id = uuid();
+      questionnaire.id = id;
+      postQuestionnaire(uid, questionnaire)
         .then((key) => {
           response.json({
             key
@@ -172,12 +226,76 @@ server.post(`${base}/questionnaires`, (request, response) => {
   });
 });
 
+server.post(`${base}/questionnaires2`, (request, response) => {
+  extractAndVerifyJwt(request).then((uid) => {
+    if (uid) {
+      const questionnaire: Questionnaire = request.body;
+      const id = uuid();
+      questionnaire.id = id;
+      postQuestionnaire2(uid, questionnaire)
+        .then(() => {
+          response.json({
+            id
+          });
+        })
+        .catch(() => {
+          response.sendStatus(500);
+        });
+    } else {
+      response.sendStatus(401);
+    }
+  });
+});
+
+// TODO: Lock down
+server.delete(
+  `${base}/questionnaires/unlist/:questionnaireId`,
+  (request, response) => {
+    extractAndVerifyJwt(request).then((uid) => {
+      if (uid) {
+        const questionnaireId = request.params.questionnaireId;
+        unListQuestionnaire(questionnaireId)
+          .then(() => {
+            response.json();
+          })
+          .catch(() => {
+            response.sendStatus(500);
+          });
+      } else {
+        response.sendStatus(401);
+      }
+    });
+  }
+);
+
+// TODO?: Does this need more locking down?
+server.put(
+  `${base}/questionnaires/list/:questionnaireId`,
+  (request, response) => {
+    extractAndVerifyJwt(request).then((verified) => {
+      if (verified) {
+        const questionnaireId = request.params.questionnaireId;
+        const questionnaire: Questionnaire = request.body;
+        publishQuestionnaire(questionnaireId, questionnaire)
+          .then(() => {
+            response.json();
+          })
+          .catch(() => {
+            response.sendStatus(500);
+          });
+      } else {
+        response.sendStatus(401);
+      }
+    });
+  }
+);
+
 server.put(`${base}/questionnaires/:questionnaireId`, (request, response) => {
-  extractAndVerifyJwt(request).then((verified) => {
-    if (verified) {
+  extractAndVerifyJwt(request).then((uid) => {
+    if (uid) {
       const questionnaireId = request.params.questionnaireId;
       const questionnaire: Questionnaire = request.body;
-      putQuestionnaire(questionnaireId, questionnaire)
+      putQuestionnaire(uid, questionnaireId, questionnaire)
         .then(() => {
           response.json();
         })
@@ -193,10 +311,10 @@ server.put(`${base}/questionnaires/:questionnaireId`, (request, response) => {
 server.delete(
   `${base}/questionnaires/:questionnaireId`,
   (request, response) => {
-    extractAndVerifyJwt(request).then((verified) => {
-      if (verified) {
+    extractAndVerifyJwt(request).then((uid) => {
+      if (uid) {
         const questionnaireId = request.params.questionnaireId;
-        deleteQuestionnaire(questionnaireId)
+        deleteQuestionnaire(uid, questionnaireId)
           .then(() => {
             response.json();
           })
